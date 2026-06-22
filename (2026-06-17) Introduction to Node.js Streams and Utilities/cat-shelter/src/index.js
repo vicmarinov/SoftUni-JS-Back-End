31fde2aa87f8a8cd810d5cb89619b362fb85054b
Victor Marinov <195519767+vicmarinov@users.noreply.github.com>
2026-06-23 02:27:24 +0300

    Set up HTTP server with initial configuration

--- (2026-06-17) Introduction to Node.js Streams and Utilities/cat-shelter/src/index.js
+++ (2026-06-17) Introduction to Node.js Streams and Utilities/cat-shelter/src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello, world!');
+import http from 'http';
+
+const SERVER_PORT = 5500;
+
+const server = http.createServer((req, res) => {
+    res.end('Hello, World!');
+});
+
+server.listen(SERVER_PORT, () => {
+    console.log(`Server is running on http://localhost:${SERVER_PORT}`);
+});
