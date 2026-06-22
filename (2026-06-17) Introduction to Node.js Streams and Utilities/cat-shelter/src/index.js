36ecffd8ceddc5cd811e03e3b29e316f6fab4638
Victor Marinov <195519767+vicmarinov@users.noreply.github.com>
2026-06-23 02:44:42 +0300

    Implement the serving of the homepage

--- (2026-06-17) Introduction to Node.js Streams and Utilities/cat-shelter/src/index.js
+++ (2026-06-17) Introduction to Node.js Streams and Utilities/cat-shelter/src/index.js
@@ -1,9 +1,25 @@
 import http from 'http';
+import fs from 'fs/promises';
 
 const SERVER_PORT = 5500;
 
-const server = http.createServer((req, res) => {
-    res.end('Hello, World!');
+const server = http.createServer(async (req, res) => {
+    if (req.url === '/styles/site.css' && req.method === 'GET') {
+        const stylesheet = await fs.readFile('./styles/site.css');
+
+        res.writeHead(200, { 'content-type': 'text/css' });
+        res.write(stylesheet);
+        res.end();
+        return;
+    }
+
+    if (req.url === '/' && req.method === 'GET') {
+        const page = await fs.readFile('./views/homepage.html');
+
+        res.writeHead(200, { 'content-type': 'text/html' });
+        res.write(page);
+        res.end();
+    }
 });
 
 server.listen(SERVER_PORT, () => {
