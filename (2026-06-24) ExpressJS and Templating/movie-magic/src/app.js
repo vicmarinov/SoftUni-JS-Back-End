35e15b2ee7592af2566018dc4145434b06c6031a
Victor Marinov <195519767+vicmarinov@users.noreply.github.com>
2026-07-06 10:57:04 +0300

    Configure Express.js

--- (2026-06-24) ExpressJS and Templating/movie-magic/src/app.js
+++ (2026-06-24) ExpressJS and Templating/movie-magic/src/app.js
@@ -1,1 +1,13 @@
-console.log('Hello, World!');
+import express from 'express';
+
+const SERVER_PORT = 5500;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Server is running on http://localhost:${SERVER_PORT}`);
+});
