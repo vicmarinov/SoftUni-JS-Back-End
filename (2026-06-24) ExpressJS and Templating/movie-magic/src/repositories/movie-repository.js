8228d91ea7e4abf85b9f7d19c320e627621d4d37
Victor Marinov <195519767+vicmarinov@users.noreply.github.com>
2026-07-06 11:58:42 +0300

    Add database and repository for movie data management

---
+++ (2026-06-24) ExpressJS and Templating/movie-magic/src/repositories/movie-repository.js
@@ -0,0 +1,21 @@
+import fs from 'fs/promises';
+
+async function readDatabase (collectionName) {
+    const content = await fs.readFile('./src/database.json');
+    const database = JSON.parse(content);
+
+    if (collectionName && !(collectionName in database)) {
+        throw new Error(`Cannot find a collection with name "${collectionName} in the database"`);
+    }
+
+    return collectionName ? database[collectionName] : database;
+}
+
+async function getAllMovies () {
+    const movies = await readDatabase('movies');
+    return movies;
+}
+
+export const movieRepository = {
+    getAll: getAllMovies
+}
